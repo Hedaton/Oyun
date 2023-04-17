8f3c3c090e0a7c2f154dfcef04ecb5bd7f0244dc
Hedaton <harunemredogan05@gmail.com>
2023-04-17 15:13:07 +0300

    bende

--- Oyun/Assets/CharacterController2D.cs
+++ Oyun/Assets/CharacterController2D.cs
@@ -6,12 +6,14 @@ public class CharacterController2D : MonoBehaviour
 {
     public float speed = 5f; // Karakterinizin hýzýný ayarlamak için kullanýn.
     public float jumpForce = 7f; // Karakterinizin zýplama kuvvetini ayarlamak için kullanýn.
+    public float fallMultiplier = 2.5f; // Karakterinizin daha hýzlý düþmesi için kullanýn.
+    public float lowJumpMultiplier = 2f; // Karakterinizin daha yavaþ düþmesi için kullanýn.
     public Transform groundCheck; // Karakterinizin yerde olup olmadýðýný kontrol etmek için kullanýn.
     public float groundCheckRadius = 0.1f; // Yer kontrolündeki yarýçapý ayarlamak için kullanýn.
     public LayerMask groundLayer; // Yer katmanýný ayarlamak için kullanýn.
 
     private Rigidbody2D rb;
-    public bool isGrounded; // Karakterinizin yerde olup olmadýðýný tutmak için kullanýn.
+    private bool isGrounded; // Karakterinizin yerde olup olmadýðýný tutmak için kullanýn.
 
     private void Start()
     {
@@ -24,13 +26,23 @@ public class CharacterController2D : MonoBehaviour
 
         Vector2 movement = new Vector2(horizontalMovement, 0f); // Sadece yatay hareketi kullanýn.
 
-        rb.velocity = movement * speed; // Rigidbody'nin hýzýný, yatay hareket deðeriyle çarpýn.
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y); // Rigidbody'nin hýzýný, yatay hareket deðeriyle ve mevcut y dikey hýzýyla birlikte ayarlayýn.
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); // Karakterinizin yerde olup olmadýðýný kontrol edin.
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space)) // Karakteriniz yerde ve Space tuþuna basýldýysa zýplayýn.
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Rigidbody'nin yukarý doðru hýzýný ayarlayýn.
+        }
+
+        // Karakteriniz havadayken daha hýzlý düþmesi veya yavaþlamasý için kodu aþaðýdaki gibi deðiþtirin.
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 }
