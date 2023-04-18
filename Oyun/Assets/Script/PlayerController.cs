6e42d68b9c6ec0dde0bc5c75e00ed14290103d9e
Hedaton <harunemredogan05@gmail.com>
2023-04-18 19:11:58 +0300

    lan oldu

--- Oyun/Assets/Script/PlayerController.cs
+++ Oyun/Assets/Script/PlayerController.cs
@@ -13,10 +13,18 @@ public class PlayerController : MonoBehaviour
     private bool isWalking = false;
     const float groundCheckRadius = 0.2f;
 
-    [SerializeField] private Animator animator;
-    [SerializeField] private Rigidbody2D rb;
+    Animator animator;
+    Rigidbody2D rb;
+
     [SerializeField] private Transform groundCheckCollider;
     [SerializeField] private LayerMask groundLayer;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
 
     private void Update()
     {
@@ -24,33 +32,44 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
             animator.SetBool("Jump", true);
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            
         }
 
 
 
-        flip();
+
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         GroundCheck();
+        
+        animator.SetFloat("yVelocity", rb.velocity.y);
         animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        flip();
+
+
+        animator.SetFloat("xValue", Mathf.Abs(rb.velocity.x));
     }
     void GroundCheck()
     {
         isGrounded = false;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
-        if(colliders.Length>0)
+        if (colliders.Length > 0)
+        {
             isGrounded = true;
+        }
+
+
         animator.SetBool("Jump", !isGrounded);
 
     }
 
     private void flip()
     {
-        if (isFacingRight && horizontal <0f || !isFacingRight && horizontal > 0f)
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
             isFacingRight = !isFacingRight;
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
