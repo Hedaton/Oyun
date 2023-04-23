6f88cb4e4550936185afc2b853d1b653844a73ef
Networkalios <116389505+Networkalios@users.noreply.github.com>
2023-04-23 13:23:42 +0300

    kamehameha başlangıç paketi

---
+++ Oyun/Assets/Script/Kamehameha.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamehameha : MonoBehaviour
+{
+
+    [SerializeField] SUPA supa;
+    
+
+    private void Update()
+    {
+        kamehameha();    
+    }
+
+    void kamehameha()
+    {
+
+    }
+
+}
